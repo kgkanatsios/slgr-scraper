47c639c47eec8edff22d15390fe8a7bf77e8123a
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-04-29 21:03:18 +0300

    Add season at Team class

--- Models/Team.py
+++ Models/Team.py
@@ -1,19 +1,27 @@
 import json
 
+from Helpers.StringHelper import *
+
 
 class Team:
     players: dict = {}
     name: str = None
+    season: str = None
     url: str = None
 
-    def __init__(self, name: str, url: str, players: dict):
+    def __init__(self, name: str, season: str, url: str, players: dict):
         self.name = name
+        self.season = season
         self.url = url
         self.players = players
+
+    def __str__(self) -> str:
+        return f"team-{sanitizeString(convertToKebabCase(self.name))}-season-{sanitizeString(convertToKebabCase(self.season))} "
 
     def toJson(self, dumps: bool = True):
         data: dict = {}
         data['name'] = self.name
+        data['season'] = self.season
         data['url'] = self.url
         data['players'] = []
 
