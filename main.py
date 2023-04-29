f3b46d1784d369f9169254efe6cc926379484da6
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-04-29 20:30:08 +0300

    Get team data

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import typer
+from rich import print
+
+from Helpers.UrlHelper import getContentFromUrl
+from Helpers.DomParserHelper import htmlParser, getPlayers, getTeamName
+
+from Models.Team import Team
 
 
-def main(prev_season_squad: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/el/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/el/teamschedule/845/21/222301/")):
-    pass
+def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/")):
+    prev_season_squad_html = getContentFromUrl(prev_season_squad_url)
+    prev_season_squad_dom = htmlParser(prev_season_squad_html)
+
+    players: list = getPlayers(prev_season_squad_dom)
+    team_name: str = getTeamName(prev_season_squad_dom)
+    prev_season_team = Team(team_name, prev_season_squad_url, players)
 
 
 if __name__ == "__main__":
