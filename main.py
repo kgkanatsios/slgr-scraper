fb2fe566baa0c42a9ef1461c9f2cd705f8708d68
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-04-29 23:43:29 +0300

    Update main to get and store team's games

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import typer
+import json
+
 from rich import print
+from rich.progress import track
 
 from Helpers.UrlHelper import getContentFromUrl
-from Helpers.DomParserHelper import htmlParser, getPlayers, getTeamName, getSeason
+from Helpers.StringHelper import filenameFormater
+from Helpers.DomParserHelper import getFullTeam, getMatchdaysLinks, getGameLink, getFullGame
 
 from Constants import LocalFoldersConstants
-
-from Models.Team import Team
 
 
 def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/")):
     prev_season_squad_html = getContentFromUrl(prev_season_squad_url)
-    prev_season_squad_dom = htmlParser(prev_season_squad_html)
-
-    players: list = getPlayers(prev_season_squad_dom)
-    team_name: str = getTeamName(prev_season_squad_dom)
-    season: str = getSeason(prev_season_squad_dom)
-    prev_season_team = Team(team_name, season, prev_season_squad_url, players)
+    prev_season_team = getFullTeam(
+        prev_season_squad_html, prev_season_squad_url)
 
     with open(LocalFoldersConstants.EXPORTS_FOLDER + str(prev_season_team) + ".json", "w") as outfile:
         outfile.write(prev_season_team.toJson())
+
+    current_season_schedule_html = getContentFromUrl(current_season_schedule)
+    team_name, matchdays_urls = getMatchdaysLinks(current_season_schedule_html)
+
+    game_urls = []
+    for matchdays_url in track(matchdays_urls, description="Fetching game urls..."):
+        matchday_html = getContentFromUrl(matchdays_url)
+        game_link = getGameLink(matchday_html)
+        if game_link is not None:
+            game_urls.append(game_link)
+
+    games = []
+    for game_url in track(game_urls, description="Fetching squads..."):
+        game_html = getContentFromUrl(game_url)
+        game = getFullGame(game_html, game_url)
+        games.append(game.toJson(False))
+
+    with open(LocalFoldersConstants.EXPORTS_FOLDER + filenameFormater(team_name) + "-games-squads.json", "w") as outfile:
+        outfile.write(json.dumps(games))
 
 
 if __name__ == "__main__":
