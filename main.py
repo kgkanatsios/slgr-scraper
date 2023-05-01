1ae1dd492c766dff76227c2c710a053d2cbcc8f7
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-05-01 17:49:06 +0300

    Update main

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import typer
 import json
 
-from rich import print
 from rich.progress import track
 
 from Helpers.UrlHelper import getContentFromUrl
-from Helpers.StringHelper import filenameFormater
 from Helpers.DomParserHelper import getFullTeam, getMatchdaysLinks, getGameLink, getFullGame
+from Helpers.FileHelper import exportToFile
 
-from Constants import LocalFoldersConstants
+from Services.SeasonComparisonService import SeasonComparisonService
 
 
-def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/")):
+def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/"), file_export: bool = typer.Option(False, "--export", help="Export data to JSON files")):
     prev_season_squad_html = getContentFromUrl(prev_season_squad_url)
     prev_season_team = getFullTeam(
         prev_season_squad_html, prev_season_squad_url)
-
-    with open(LocalFoldersConstants.EXPORTS_FOLDER + str(prev_season_team) + ".json", "w") as outfile:
-        outfile.write(prev_season_team.toJson())
 
     current_season_schedule_html = getContentFromUrl(current_season_schedule)
     team_name, matchdays_urls = getMatchdaysLinks(current_season_schedule_html)
@@ -33,10 +29,17 @@ def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of th
     for game_url in track(game_urls, description="Fetching squads..."):
         game_html = getContentFromUrl(game_url)
         game = getFullGame(game_html, game_url)
-        games.append(game.toJson(False))
+        games.append(game)
 
-    with open(LocalFoldersConstants.EXPORTS_FOLDER + filenameFormater(team_name) + "-games-squads.json", "w") as outfile:
-        outfile.write(json.dumps(games))
+    season_comparison = SeasonComparisonService(prev_season_team, games)
+    season_comparison_result = season_comparison.getComparison()
+
+    if file_export:
+        exportToFile(team_name + "-games-squads",
+                     json.dumps(list(map(lambda game: game.toJson(False), games))))
+        exportToFile(prev_season_team, prev_season_team.toJson())
+        exportToFile(team_name + '-season-comparison',
+                     json.dumps(season_comparison_result))
 
 
 if __name__ == "__main__":
