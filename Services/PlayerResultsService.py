053677d771a53f11fa8c5fcada57499081c26ffc
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-05-01 19:57:58 +0300

    Add player results service

--- Services/PlayerResultsService.py
+++ Services/PlayerResultsService.py
@@ -1,44 +1,34 @@
-from Models.Team import Team
 from Models.Game import Game
 
 from Enums.GameResultEnum import GameResultEnum
 
 
-class SeasonComparisonService:
-    prev_season_team: Team
+class PlayerResultsService:
+    team_name: str
     current_season_games: list
-    result: list
+    result: dict
 
-    def __init__(self, prev_season_team: Team, current_season_games: list):
-        self.prev_season_team = prev_season_team
+    def __init__(self, team_name: str, current_season_games: list):
+        self.team_name = team_name
         self.current_season_games = current_season_games
 
-    def getComparison(self):
-        self.result = []
-        prev_season_players = self.prev_season_team.players
+    def getPlayerResults(self):
+        self.result = {}
         for current_season_game in self.current_season_games:
-            game_stats: dict = {}
             label = self.__getLabel(current_season_game)
             score_label = self.__getResultLabel(current_season_game).value
 
-            game_stats["label"] = label
-            game_stats["score_label"] = score_label
-
             squad = self.__getSquad(current_season_game)
-            prev_season_players_in_game = []
             for player in squad:
-                for prev_season_player in prev_season_players:
-                    if prev_season_player.url == player.url:
-                        prev_season_players_in_game.append(
-                            player.toJson(False))
-                        break
+                stats: dict = {}
+                if player.url not in self.result.keys():
+                    self.result[player.url]: list = []
 
-            game_stats["prev_season_players"] = prev_season_players_in_game
-            game_stats["prev_season_players_num"] = len(
-                prev_season_players_in_game)
-            game_stats["prev_season_players_percent"] = self.__CalcPercent(
-                len(squad), len(prev_season_players_in_game))
-            self.result.append(game_stats)
+                stats["name"] = player.name
+                stats["label"] = label
+                stats["score_label"] = score_label
+
+                self.result[player.url].append(stats)
 
         return self.result
 
@@ -46,7 +36,7 @@ class SeasonComparisonService:
         home_team: str = game.home_team
         opponent_name: str = game.home_team
         home_away_prefix: str = "(A)"
-        if home_team in self.prev_season_team.name:
+        if home_team in self.team_name:
             opponent_name: str = game.guest_team
             home_away_prefix: str = "(H)"
 
@@ -54,7 +44,7 @@ class SeasonComparisonService:
 
     def __getSquad(self, game: Game) -> list:
         home_team: str = game.home_team
-        if home_team in self.prev_season_team.name:
+        if home_team in self.team_name:
             return game.home_players
 
         return game.guest_players
@@ -62,7 +52,7 @@ class SeasonComparisonService:
     def __getResultLabel(self, game: Game) -> GameResultEnum:
         home_team: str = game.home_team
 
-        if home_team in self.prev_season_team.name:
+        if home_team in self.team_name:
             my_score: int = game.home_score
             opponent_score: int = game.guest_score
         else:
@@ -76,12 +66,3 @@ class SeasonComparisonService:
             return GameResultEnum.WIN
 
         return GameResultEnum.LOSE
-
-    def __CalcPercent(self, total: int, part: int):
-        result: float = 0
-        if (total == 0):
-            return result
-
-        result = (part * 100) / total
-
-        return result
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from rich.progress import track
 
 from Helpers.UrlHelper import getContentFromUrl
 from Helpers.DomParserHelper import getFullTeam, getMatchdaysLinks, getGameLink, getFullGame
-from Helpers.FileHelper import exportToFile
+from Helpers.FileHelper import exportToFile, exportToJsonFile
 
 from Services.SeasonComparisonService import SeasonComparisonService
+from Services.PlayerResultsService import PlayerResultsService
 
 
 def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/"), file_export: bool = typer.Option(False, "--export", help="Export data to JSON files")):
@@ -34,12 +35,17 @@ def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of th
     season_comparison = SeasonComparisonService(prev_season_team, games)
     season_comparison_result = season_comparison.getComparison()
 
+    players_stats = PlayerResultsService(team_name, games)
+    players_stats_result = players_stats.getPlayerResults()
+
     if file_export:
         exportToFile(team_name + "-games-squads",
                      json.dumps(list(map(lambda game: game.toJson(False), games))))
         exportToFile(prev_season_team, prev_season_team.toJson())
-        exportToFile(team_name + '-season-comparison',
+        exportToFile(team_name + "-season-comparison",
                      json.dumps(season_comparison_result))
+        exportToJsonFile(team_name + "-player-results",
+                         json.dumps(players_stats_result))
 
 
 if __name__ == "__main__":
