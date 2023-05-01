367d4dcea6cd47dfb42d3b2c5d1938ead802028f
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-05-01 17:47:53 +0300

    Export data to json and excel file

--- Helpers/FileHelper.py
+++ Helpers/FileHelper.py
@@ -1,7 +1,20 @@
+import pandas as pd
+
 from Constants import LocalFoldersConstants
 from Helpers.StringHelper import filenameFormater
 
 
-def exportToFile(filename: str, ext: str, content: str) -> None:
-    with open(LocalFoldersConstants.EXPORTS_FOLDER + filenameFormater(str(filename)) + "." + ext, "w") as outfile:
+def exportToFile(filename: str, content: str) -> None:
+    exportToJsonFile(filename, content)
+    exportToExcelFile(filename, content)
+
+
+def exportToJsonFile(filename: str, content: str) -> None:
+    with open(LocalFoldersConstants.EXPORTS_FOLDER + filenameFormater(str(filename)) + ".json", "w") as outfile:
         outfile.write(content)
+
+
+def exportToExcelFile(filename: str, content: str) -> None:
+    df = pd.read_json(content)
+    df.to_excel(LocalFoldersConstants.EXPORTS_FOLDER +
+                filenameFormater(str(filename)) + ".xlsx")
