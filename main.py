5c96fb6575e3fa2b3b2a5b428946d8957d71e807
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-05-02 23:27:51 +0300

    Enhance UX

--- main.py
+++ main.py
@@ -1,17 +1,92 @@
 import typer
 import json
 
-from rich.progress import track
+import inquirer
+
+from rich.progress import track, Progress, SpinnerColumn, TextColumn
 
 from Helpers.UrlHelper import getContentFromUrl
-from Helpers.DomParserHelper import getFullTeam, getMatchdaysLinks, getGameLink, getFullGame
+from Helpers.DomParserHelper import getFullTeam, getMatchdaysLinks, getGameLink, getFullGame, getAllTeams, getTeamScheduleUrl, getTeamSquadUrl
 from Helpers.FileHelper import exportToFile, exportToJsonFile
 
 from Services.SeasonComparisonService import SeasonComparisonService
 from Services.PlayerResultsService import PlayerResultsService
 
+from Constants.UrlConstants import HOME_PAGE_URL
 
-def main(prev_season_squad_url: str = typer.Argument(..., help="Team's URL of the previous season squad. Example: https://www.slgr.gr/en/team/785/teamComp/20/"), current_season_schedule: str = typer.Argument(..., help="Team's URL of the current season schedule. Example: https://www.slgr.gr/en/teamschedule/845/21/222301/"), file_export: bool = typer.Option(False, "--export", help="Export data to JSON files")):
+
+def main():
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Initializing...", total=None)
+        home_page_html = getContentFromUrl(HOME_PAGE_URL)
+        teams = getAllTeams(home_page_html)
+
+    team_question = [
+        inquirer.List('team',
+                      message="Select the team:",
+                      choices=teams.values(),
+                      ),
+    ]
+    team_answer = inquirer.prompt(team_question)
+    team = {key: value for (
+        key, value) in teams.items() if value == team_answer['team']}
+    team_url = list(team.keys())[0]
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Get team's data...", total=None)
+        team_html = getContentFromUrl(team_url)
+
+        team_squad_url = getTeamSquadUrl(team_html)
+        team_squad_html = getContentFromUrl(team_squad_url)
+        team = getFullTeam(team_squad_html, team_squad_url)
+
+        team_schedule_url = getTeamScheduleUrl(team_html)
+        team_schedule_html = getContentFromUrl(team_schedule_url)
+        team_name, matchdays_urls = getMatchdaysLinks(team_schedule_html)
+
+    game_urls = []
+    for matchdays_url in track(matchdays_urls, description="Fetching game urls..."):
+        matchday_html = getContentFromUrl(matchdays_url)
+        game_link = getGameLink(matchday_html)
+        if game_link is not None:
+            game_urls.append(game_link)
+
+    games = []
+    for game_url in track(game_urls, description="Fetching squads..."):
+        game_html = getContentFromUrl(game_url)
+        game = getFullGame(game_html, game_url)
+        games.append(game)
+
+    service_question = [
+        inquirer.Checkbox(
+            "services",
+            message="What are you interested in?",
+            choices=[("All the games", "all_games"), ("Results per player", "player_results"),
+                     ("Compare squad with previous season", "season_comparison")],
+            default=["all_games", "player_results"],
+        ),
+    ]
+    service_answer = inquirer.prompt(service_question)
+
+    if "all_games" in service_answer["services"]:
+        exportToFile(team_name + "-games-squads",
+                     json.dumps(list(map(lambda game: game.toJson(False), games))))
+
+    if "player_results" in service_answer["services"]:
+        players_stats = PlayerResultsService(team, games)
+        players_stats_result = players_stats.getPlayerResults()
+        exportToJsonFile(team_name + "-player-results",
+                         json.dumps(players_stats_result))
+
+    return
     prev_season_squad_html = getContentFromUrl(prev_season_squad_url)
     prev_season_team = getFullTeam(
         prev_season_squad_html, prev_season_squad_url)
