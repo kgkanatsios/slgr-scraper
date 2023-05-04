16860b8e965276a1adba12401e8f59b03e31ead8
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2023-05-04 21:23:27 +0300

    Update file helper: print file path

--- Helpers/FileHelper.py
+++ Helpers/FileHelper.py
@@ -5,16 +5,24 @@ from Helpers.StringHelper import filenameFormater
 
 
 def exportToFile(filename: str, content: str) -> None:
-    exportToJsonFile(filename, content)
-    exportToExcelFile(filename, content)
+    path = exportToJsonFile(filename, content)
+    print("Results: " + path)
 
 
-def exportToJsonFile(filename: str, content: str) -> None:
-    with open(LocalFoldersConstants.EXPORTS_FOLDER + filenameFormater(str(filename)) + ".json", "w") as outfile:
+def exportToJsonFile(filename: str, content: str) -> str:
+    file_path = LocalFoldersConstants.EXPORTS_FOLDER + \
+        filenameFormater(str(filename)) + ".json"
+    with open(file_path, "w") as outfile:
         outfile.write(content)
 
+    return file_path
 
-def exportToExcelFile(filename: str, content: str) -> None:
+
+def exportToExcelFile(filename: str, content: str) -> str:
+    file_path = LocalFoldersConstants.EXPORTS_FOLDER + \
+        filenameFormater(str(filename)) + ".xlsx"
+
     df = pd.read_json(content)
-    df.to_excel(LocalFoldersConstants.EXPORTS_FOLDER +
-                filenameFormater(str(filename)) + ".xlsx")
+    df.to_excel(file_path)
+
+    return file_path
